6cfc747ba9037eb3108fa66dab9ed26b62a14c57
MonsterDruide1 <5958456@gmail.com>
2025-03-02 23:12:30 +0100

    Add script and workflow for generating cache archives

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # OdysseyDecompToolsCache
-Prebuilt binaries of tools and compilers for [OdysseyDecomp](https://github.com/MonsterDruide1/OdysseyDecomp)
+Prebuilt binaries of tools and compilers for [OdysseyDecomp](https://github.com/MonsterDruide1/OdysseyDecomp).
 
-Currently this repo has two components:
-  * Pre installed binaries for Linux:
-      * `clang` 3.9.1 from `bin/clang` of [this release](https://releases.llvm.org/3.9.1/clang+llvm-3.9.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz) from the llvm releases
-      * `lld` 4.0.1 from `bin/ld.lld` of [this release](https://releases.llvm.org/4.0.1/clang+llvm-4.0.1-x86_64-linux-gnu-Fedora-25.tar.xz)
-      * The `check`, `listsym` and `decompme` binaries from a pre-built version of [viking](https://github.com/open-ead/nx-decomp-tools/tree/master/viking) for Linux
-  * llvm 3.9.1 libc++ headers from the `include/c++/v1` folder of the clang 3.9.1 Linux release above
+See [Releases](https://github.com/MonsterDruide1/OdysseyDecompToolsCache/releases) for downloads of the respective pre-generated files.
+
+Two separate components are available:
+- Prebuilt binaries for Linux (`OdysseyDecomp-binaries_Linux.tar.gz`):
+  - `clang` from `bin/clang` of the [official release of Clang 3.9.1 for Ubuntu 16.04](https://releases.llvm.org/3.9.1/clang+llvm-3.9.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz)
+  - `lld` from `bin/ld.lld` of the [official release of Clang 4.0.1 for Fedora 25](https://releases.llvm.org/4.0.1/clang+llvm-4.0.1-x86_64-linux-gnu-Fedora-25.tar.xz)
+  - `check`, `listsym` and `decompme` from building [viking](https://github.com/open-ead/nx-decomp-tools/tree/master/viking)
+- libc++ headers from Clang 3.9.1 (`OdysseyDecomp-libcxx-headers.tar.gz`):
+  - `include` obtained from `include/c++/v1` of Clang 3.9.1, as mentioned above
---
+++ generate.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# fail on first error
+set -e
+
+# create directory for files being downloaded
+mkdir -p download
+
+# download clang 3.9.1
+pushd download
+if [ ! -f "clang+llvm-3.9.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz" ]; then
+    wget https://releases.llvm.org/3.9.1/clang+llvm-3.9.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz
+fi
+popd
+
+# extract clang 3.9.1
+pushd download
+if [ ! -d "clang+llvm-3.9.1-x86_64-linux-gnu-ubuntu-16.04" ]; then
+    tar xf clang+llvm-3.9.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz
+fi
+popd
+
+# download clang 4.0.1
+pushd download
+if [ ! -f "clang+llvm-4.0.1-x86_64-linux-gnu-Fedora-25.tar.xz" ]; then
+    wget https://releases.llvm.org/4.0.1/clang+llvm-4.0.1-x86_64-linux-gnu-Fedora-25.tar.xz
+fi
+popd
+
+# extract clang 4.0.1
+pushd download
+if [ ! -d "clang+llvm-4.0.1-x86_64-linux-gnu-Fedora-25" ]; then
+    tar xf clang+llvm-4.0.1-x86_64-linux-gnu-Fedora-25.tar.xz
+fi
+popd
+
+# build decomp tools
+pushd nx-decomp-tools/viking
+cargo build --manifest-path Cargo.toml --release
+popd
+
+
+# create/clear build directory
+if [ -d "build" ]; then
+    rm -rf build
+fi
+mkdir build
+
+# create folder skeleton
+mkdir -p build/OdysseyDecomp-binaries_Linux/bin
+mkdir -p build/OdysseyDecomp-libcxx-headers/include
+
+# copy clang 3.9.1
+cp download/clang+llvm-3.9.1-x86_64-linux-gnu-ubuntu-16.04/bin/clang build/OdysseyDecomp-binaries_Linux/bin/
+
+# copy lld from clang 4.0.1
+cp download/clang+llvm-4.0.1-x86_64-linux-gnu-Fedora-25/bin/ld.lld build/OdysseyDecomp-binaries_Linux/bin/
+
+# copy viking tools
+cp nx-decomp-tools/viking/target/release/check build/OdysseyDecomp-binaries_Linux/bin/
+cp nx-decomp-tools/viking/target/release/listsym build/OdysseyDecomp-binaries_Linux/bin/
+cp nx-decomp-tools/viking/target/release/decompme build/OdysseyDecomp-binaries_Linux/bin/
+
+# copy libc++ from clang 3.9.1
+cp -r download/clang+llvm-3.9.1-x86_64-linux-gnu-ubuntu-16.04/include/c++/v1/* build/OdysseyDecomp-libcxx-headers/include
+
+
+# create tarball
+pushd build/OdysseyDecomp-binaries_Linux
+tar -cJf ../OdysseyDecomp-binaries_Linux.tar.xz *
+popd
+pushd build/OdysseyDecomp-libcxx-headers
+tar -cJf ../OdysseyDecomp-libcxx-headers.tar.xz *
+popd
